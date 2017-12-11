63577b361f8e2e9300e0e410cf48a3eb6a1613e2
52lions06 <mckelvey.william@yahoo.com>
2017-12-10 21:46:42 -0500

    installed passport and google passport strategy

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 'use strict';
-
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oath20').Strategy;
 
 const app = express();
 
-app.get( '/', ( req, res ) => {
-  res.send({ hi: 'Yooooo this works' });
-});
+passport.use( new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
