af3154501d9861ea72e36edc4f325ee4a307e0f3
52lions06 <mckelvey.william@yahoo.com>
2017-12-12 12:28:59 -0500

    added cookie-session/ can manage users by enabling cookies

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 'use strict';
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+
+require('./models/User');
 require('./services/passport'); //Don't need Const because I am not reusing it
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, // max time before cookie expires in ms
+    keys: [keys.cookieKey]
+  })
+);
+
+// This is how the cookie session is activated 
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect(keys.mongoURI);
 
 //exporting a function so only need to call it with "app" to use the routes
 require('./routes/authRoutes') (app); 
