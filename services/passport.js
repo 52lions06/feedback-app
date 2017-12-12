aef70b307f89d6040161201039fe28874a5976b3
52lions06 <mckelvey.william@yahoo.com>
2017-12-12 12:30:31 -0500

    strategy can search for existing user / able to serialize and deserialize models

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,18 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id); //we are grabbing the mongo id from the user record we created 
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id) //still using the mongo id because we may have multiple strategies
+    .then((user) => done(null, user));
+});
 
 passport.use(
   new GoogleStrategy(
@@ -10,9 +22,18 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log('access Token:', accessToken);
-      console.log('refresh Token: ', refreshToken);
-      console.log('profile: ', profile);
+      User.findOne({ googleID: profile.id })
+        .then((existingUser) => {
+          if (existingUser) {
+            // then nothing should happen
+            done(null, existingUser);
+          } else {
+            // other wise we should be having a new id made and saved 
+            new User({ googleID: profile.id })
+              .save()
+              .then((user) => done(null, user));
+          }
+        });
     }
   )
 );
