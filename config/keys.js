6e54eae0c1d8b3a7efe3ee5f7c496df29a2b83aa
52lions06 <mckelvey.william@yahoo.com>
2017-12-14 11:29:51 -0500

    added dev and prod env

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+// Where we determine if we are in the production or development environment 
+
+if( process.env.NODE_ENV === 'production') {
+// this will determine that our production environment 
+} else {
+// this will determine our development environment 
+}
