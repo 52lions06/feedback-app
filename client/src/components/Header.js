c685a24348a92caebb53390be234e56cd9371dd2
52lions06 <mckelvey.william@yahoo.com>
2017-12-16 22:38:40 -0500

    provide Header design with materialize CSS

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,9 +3,18 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     return(
-      <div>
-        Header is Here!
-      </div>
+      <nav>
+        <div className="nav-wrapper">
+          <a href="left brand-logo">
+            Email Magic
+          </a>
+          <ul className="right">
+            <li>
+              <a>Login with Google</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     )
   }
 }
