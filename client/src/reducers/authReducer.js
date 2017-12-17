d87a241d106de295ce502bcebb5c94d0f79fb75a
52lions06 <mckelvey.william@yahoo.com>
2017-12-16 21:30:17 -0500

    added redux files

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,10 @@
+export default function( state={}, action ) {
+  switch(action.type) {
+    default:
+    return state;
+  }
+}
+
+// the initial setup has an empty state and empty action object as parameters
+
+// we are return the empty state with a default value because otherwise state would return "undefined"
