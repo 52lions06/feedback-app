1430a8477f56fe6acb671aeb8875db9bb38acdb1
52lions06 <mckelvey.william@yahoo.com>
2017-12-16 21:29:48 -0500

    deleted default files installed redux

---
+++ client/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <div>
+      This Works!
+    </div>
+  )
+};
+
+export default App;
