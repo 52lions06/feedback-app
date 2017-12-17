424b17b1914faf3afe989d35014052b3d942ff5d
52lions06 <mckelvey.william@yahoo.com>
2017-12-16 22:29:07 -0500

    added React Router and components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
+import Header from './Header';
+
+
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>SurveyNew</h2>
+const LandingPage = () => <h2>LandingPage</h2>
+
 const App = () => {
   return (
     <div>
-      This Works!
+     <BrowserRouter>
+      <div>
+        <Header />
+        <Route exact path="/" component={LandingPage}/>
+        <Route exact path="/surveys" component={Dashboard} />
+        <Route path="/surveys/new" component={SurveyNew} />
+      </div>
+     </BrowserRouter>
     </div>
   )
 };
---
+++ client/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return(
+      <div>
+        Header is Here!
+      </div>
+    )
+  }
+}
+
+export default Header;
