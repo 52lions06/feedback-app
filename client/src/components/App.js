44a1177f13493464b679872013f202073d5fe771
52lions06 <mckelvey.william@yahoo.com>
2017-12-18 14:53:21 -0500

    rendering fetchUser action and added connect for react-redux

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-
+import { connect } from 'react-redux';
 import Header from './Header';
+import * as actions from '../actions';
 
 
 const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
 const LandingPage = () => <h2>LandingPage</h2>
 
-const App = () => {
-  return (
-    <div className="container">
-     <BrowserRouter>
-      <div>
-        <Header />
-        <Route exact path="/" component={LandingPage}/>
-        <Route exact path="/surveys" component={Dashboard} />
-        <Route path="/surveys/new" component={SurveyNew} />
+class App extends React.Component {
+  componentDidMount(){
+    this.props.fetchUser();
+  }  
+
+  render() {
+    return (
+      <div className="container">
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/" component={LandingPage}/>
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route path="/surveys/new" component={SurveyNew} />
+        </div>
+      </BrowserRouter>
       </div>
-     </BrowserRouter>
-    </div>
-  )
+    ) 
+  }
 };
 
-export default App;
+export default connect(null, actions) (App);
