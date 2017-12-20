bd3b0d67d8c997c41b8780ba6583e33d09b79620
52lions06 <mckelvey.william@yahoo.com>
2017-12-20 17:18:46 -0500

    brought in handleToken action

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 class Payments extends React.Component {
   render(){
@@ -8,7 +10,7 @@ class Payments extends React.Component {
           name="Email Magic"
           description="pay $5 for 5 credits"
           amount={500}
-          token={ token => console.log(token)} //token is not an actual token its an object of information 
+          token={ token => this.props.handleToken(token)} //token is not an actual token its an object of information 
           stripeKey={process.env.REACT_APP_STRIPE_KEY}
           >
           <button className="btn">ADD CREDITS</button>
@@ -17,4 +19,6 @@ class Payments extends React.Component {
   }
 }
 
-export default Payments;
+export default connect(null, actions)(Payments);
+
+//brought in the handleToken action so we could post to the back end
