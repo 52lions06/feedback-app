6d02ff42d2c4638ed87009f6880592c556dc50be
52lions06 <mckelvey.william@yahoo.com>
2017-12-22 11:30:29 -0500

    added logic so express can prepare for deployement to production

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ require('./services/passport'); //Don't need Const because I am not reusing it
 
 const app = express();
 
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000, // max time before cookie expires in ms
@@ -26,9 +26,18 @@ app.use(passport.session());
 mongoose.connect(keys.mongoURI);
 
 //exporting a function so only need to call it with "app" to use the routes
-require('./routes/authRoutes') (app); 
-require('./routes/billingRoutes') (app); 
+require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  // we want Express to be able to access main.js or main.css
+  app.use(express.static('client/build'));
+  // we want Express to be able to access index.html if there is a route foreign to Express 
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
