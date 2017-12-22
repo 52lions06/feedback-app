c7e897233ca8dae8585ca11b13c6af680015241b
52lions06 <mckelvey.william@yahoo.com>
2017-12-22 16:38:48 -0500

    require in survey model properties

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,8 +1,26 @@
+const mongoose = require('mongoose');
 const requireLogin = require('../middleware/requireLogin');
 const requireCredits = require('../middleware/requireCredits');
 
+const Survey = mongoose.model('surveys');
+
 module.exports = app => {
   app.post( '/api/surveys', requireLogin, requireCredits, (req, res) => {
+    const { title, body, subject, recipients } = req.body;
 
+    const survey = new Survey({
+      title, 
+      body,
+      subject,
+
+    });
   });
 };
+
+
+// inside of survey can also be written like 
+    // title: title, 
+    // body: body,
+    // subject: subject,
+
+//USING ES6 to refactor it
